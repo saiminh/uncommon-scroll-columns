0fee0d5c04ae695fb002999a4c0817c9b5ca11d7
saiminh <saiminh@gmail.com>
2023-06-19 11:40:27 +0200

    allow space for nav

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fe321125ac4d683cc769');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd3d3c49bb322c7b6edb9');
